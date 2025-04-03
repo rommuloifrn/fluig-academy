0cc237c2e5f5c49e88efa1d198404ef05d01802b
rommuloifrn <roomulo.cavalkant@outlook.com>
2025-04-03 13:26:43 -0300

    feat: adiciona dataset "dsMoedas" e usa ele para pegar as moedas para opções no formulário

---
+++ datasets/dsMoedas.js
@@ -0,0 +1,21 @@
+function defineStructure() {
+
+}
+function onSync(lastSyncDate) {
+
+}
+function createDataset(fields, constraints, sortFields) {
+	var ds = DatasetBuilder.newDataset();
+	
+	ds.addColumn("simbolo")
+	ds.addColumn("nomeMoeda")
+	
+	ds.addRow(new Array("R$", "Real"))
+	ds.addRow(new Array("US$", "Dólar Americano"))
+	ds.addRow(new Array("U$", "Peso Uruguaio"))
+	
+	return ds;
+
+}function onMobileSync(user) {
+
+}
